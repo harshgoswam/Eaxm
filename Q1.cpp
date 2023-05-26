b291b29a71eb78633a70e2a9a1301c37e4207cf0
harshgoswam <122149827+harshgoswam@users.noreply.github.com>
2023-05-26 11:50:41 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+main()
+{
+	int id=;
+	char password[50];
+	int item_no;
+	char item_name[50];
+	int quantity;
+	int tax;
+	int discount;
+	
+	int id2;
+	
+	
+}
+
+try
+
+{
+	
+	if(strcmp(id,))
+	
+}
+
